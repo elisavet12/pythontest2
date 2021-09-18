76e52ca6fb0ba784cca2d37b7e87ff36d53263cc
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-09-18 16:37:13 +0300

    solution2

---
+++ solution2.py
@@ -0,0 +1,18 @@
+# Convert one type to another:
+x = 8    # int
+y = 1.3  # float
+# 1. Convert from int to float:
+a=float(x)
+print(a)
+# 2. Convert from float to int:
+b=int(y)
+print(b)
+# 3. Write a Python program to convert an integer to binary.
+#1st option
+x=11
+a=bin(x)
+print(a)
+
+#2nd option
+x=format(11, "b")
+print(x)
